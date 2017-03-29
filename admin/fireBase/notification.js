db1634fc6a4b6f4f0971e67774b05470daaa4e7e
SVISHNEV <shlomo-stanisla.vishnevetskiy@amdcocs.com>
2017-03-29 20:50:59 +0300

    push notification poc

---
+++ admin/fireBase/notification.js
@@ -0,0 +1,40 @@
+var admin = require("firebase-admin");
+
+var serviceAccount = require("./firebaseKey.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://moni-tor.firebaseio.com"
+    });
+
+    // This registration token comes from the client FCM SDKs.
+//var registrationToken = "fmH08v1XQOo:APA91bH1ZmAQEsKQGlw9gKWwZ_Ryrr1ZCFlkbIOI7VPuaoTbA5tzSVM5RwyZRFKpkDZfjs-5tn_cNQHXWAQyNMmdwI8MkTGZOmkOJCXjjcq_BWRKsag80_wAFR9HbQll_aNRLSTB4-7n";
+
+function sendNotification(token) {
+
+    //var token = 'fmH08v1XQOo:APA91bH1ZmAQEsKQGlw9gKWwZ_Ryrr1ZCFlkbIOI7VPuaoTbA5tzSVM5RwyZRFKpkDZfjs-5tn_cNQHXWAQyNMmdwI8MkTGZOmkOJCXjjcq_BWRKsag80_wAFR9HbQll_aNRLSTB4-7n';
+
+    // See the "Defining the message payload" section below for details
+    // on how to define a message payload.
+    var payload = {
+    data: {
+        title: "TITLE",
+        msg: "MESSAGE",
+        sub_text: "run run run"
+    }
+    };
+
+    // Send a message to the device corresponding to the provided
+    // registration token.
+    admin.messaging().sendToDevice(token, payload)
+    .then(function(response) {
+        // See the MessagingDevicesResponse reference documentation for
+        // the contents of response.
+        console.log("Successfully sent message:", response);
+    })
+    .catch(function(error) {
+        console.log("Error sending message:", error);
+    });
+}
+
+module.exports = sendNotification;
--- admin/server.js
+++ admin/server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+var sendNotification = require('./fireBase/notification.js');
+
 var app = express();
 app.use(bodyParser.json());
 
@@ -29,6 +31,11 @@ app.use(function (req, res, next) {
 
 
 app.use('/dist', express.static(__dirname + '/dist'));
+
+app.post('/notify', function(request, response) {
+    sendNotification(request.body.token);
+    response.json({a: 'hello'});
+});
 
 app.get('/', function(request, response) {
   response.sendFile(__dirname + '/index.html');
