d56497c775f12c40914e27b76c8c21909da820a5
navke <ewk_80@server.fake>
2017-03-29 19:48:16 +0300

    admin screen first draft - better ui

--- admin/app/components/MonitorAdmin.jsx
+++ admin/app/components/MonitorAdmin.jsx
@@ -1,14 +1,27 @@
 import React, {Component} from 'react';
 
 var interval;
+
+function calcTimeDiff(date1, date2) {
+	/*const date1Ms = date1.getTime();
+	const date2Ms = date2.getTime();
+
+	let differenceMs = date2Ms - date1Ms;
+	differenceMs = differenceMs/1000;
+	differenceMs = differenceMs/60;
+	const minutes = Math.floor(differenceMs % 60);*/
+	return 1;
+}
 class MonitorAdmin extends Component {
 	state = {
-		showModal: false
+		showModal: false,
+		date: new Date()
 	}
 
 	componentDidMount() {
 		this.props.fetchQueue();
-		interval = setInterval(this.props.fetchQueue,60000);
+		this.setState({date: new Date()});
+		interval = setInterval(this.props.fetchQueue,10000);
 	}
 	componentWillUnmount() {
 		clearInterval(interval);
@@ -24,10 +37,11 @@ class MonitorAdmin extends Component {
 			<div>
 				<div className='content'>
 					<div className='logo'></div>
-					<div className='main-actions'><button onClick={this.props.invitePatient}>זמן את הבאה בתור</button>
-						<button onClick={() => this.setState({showModal: true})} >הוסף מטופלת לרשימה</button>
+					<div className='date'>{this.state.date.toDateString()}</div>
+					<div className='main-actions'><button disabled={this.props.isListEmpty} onClick={this.props.invitePatient}>זמן את הבאה בתור</button>
 					</div>
-					<List {...this.props}></List>
+					<div className='action'><button onClick={() => this.setState({showModal: true})} >הוסף מטופלת</button></div>
+					<List {...this.props} date={this.state.date}></List>
 				</div>
 				{this.state.showModal && <div className='modal-bg'>	</div>}
 				{this.state.showModal && <AddPatient {...this.props.patient} addEnabled={this.props.allowCreateNew}
@@ -46,43 +60,54 @@ class AddPatient extends Component  {
 			<div className='field'><label>שם המטופלת:</label><input type='text' id='fullName' name='fullName' value={fullName} onChange={(e) => this.props.updatePatientData({fullName: e.target.value})}/></div>
 			<div className='field'><label>ת.ז.:</label><input type='text' id='name' name='id' value={id}  onChange={(e) => this.props.updatePatientData({id: e.target.value})} /></div>
 			<div className='field'><label>מספר טלפון:</label><input type='text' id='phone' name='phone' value={phone}  onChange={(e) => this.props.updatePatientData({phone: e.target.value})}/></div>
-			<button onClick={()=>this.props.addPatient({fullName, id, phone})} disabled={!this.props.addEnabled}>אישור</button>
-			<button onClick={this.props.closeModal} >ביטול</button>
+			<div className='main-actions'>
+				<button onClick={()=>this.props.addPatient({fullName, id, phone})} disabled={!this.props.addEnabled}>אישור</button>
+				<button onClick={this.props.closeModal} >ביטול</button>
+			</div>
 		</div>);
 	}
 }
 const ListItem = (props) => {
 	const {itemData : {fullName, id, phone, registrationTime, notificationTime}} = props;
+	let regDate = new Date(registrationTime);
+	var diff =  calcTimeDiff(regDate, props.date);
+	let timeClass = '';
+	if (diff >= 60) { //60min
+		timeClass = 'red';
+	} else if (diff >= 30) {//30min
+		timeClass = 'orange';
+	}
 	return(<li className='row'>
 		<div className='big-col'>{fullName}</div>
 		<div className='big-col'>{id}</div>
 		<div className='big-col phone-number'>{phone}</div>
-		<div className='small-col'>{registrationTime}</div>
+		<div className={timeClass + ' small-col'}>{new Date(registrationTime).toTimeString().substring(0,5)}</div>
 		<div className='small-col'>{notificationTime ? 'הזמנה נשלחה' : 'ממתינה'}</div>
 		<div className='big-col flex-col'>
-			{notificationTime ?  <div className='arrived'  onClick={()=>props.removeFromQueue(id)} >המטופלת הגיעה</div> : <div ></div> }
-			<div className='remove'  onClick={()=>props.removeFromQueue(id)} >remove</div>
-			<div className='top' onClick={()=>props.moveTopQueue(id)}>top</div>
+			{notificationTime ?  <div className='action arrived'  onClick={()=>props.removeFromQueue(id)} >&#10003;</div> : <div ></div> }
+			<div className='action remove'  onClick={()=>props.removeFromQueue(id)} >&#9249;</div>
+			<div className='action top' onClick={()=>props.moveTopQueue(id)}>&#5839;</div>
 		</div>
 	</li>);
 }
 
 const ListItemHeader = (props) => {
-	return(<li key='header-key' className='row'>
-		<div className='big-col'>שם המטופלת</div>
-		<div className='big-col'>תעודת זהות</div>
-		<div className='big-col'>טלפון</div>
-		<div className='small-col'>שעת הבקשה</div>
-		<div className='small-col'>סטטוס</div>
-		<div className='big-col'>פעולות</div>
+	return(<li key='header-key' className='row header'>
+		<div className='col-title big-col'>שם המטופלת</div>
+		<div className='col-title big-col'>תעודת זהות</div>
+		<div className='col-title big-col'>טלפון</div>
+		<div className='col-title small-col'>שעת הבקשה</div>
+		<div className='col-title small-col'>סטטוס</div>
+		<div className='col-title big-col'>פעולות</div>
 	</li>);
 }
 
 const List = (props) => {
 	const actions = {invitePatient: props.invitePatient, removeFromQueue: props.removeFromQueue,  moveTopQueue: props.moveTopQueue};
 	return (<ul className='list'>
-		<ListItemHeader/>
-		{props.data && props.data.map(entry => <ListItem itemData={entry} key={entry.id}  {...actions} />)}
+		{!props.isListEmpty && <ListItemHeader/>}
+		{props.isListEmpty && <li className='row'>רשימת ההמתנה ריקה</li>}
+		{props.data && props.data.map(entry => <ListItem itemData={entry} key={entry.id} date={props.date} {...actions} />)}
 	</ul>);
 }
 
