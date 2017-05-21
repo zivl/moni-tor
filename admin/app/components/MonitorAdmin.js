2d2d53703ddd4533659f641f8df89d9158063b98
navke <ewk_80@server.fake>
2017-05-21 10:11:20 +0300

    admin screen - bug fixes and style fixes - such as date, timediff, added hover tooltip

--- admin/app/components/MonitorAdmin.js
+++ admin/app/components/MonitorAdmin.js
@@ -2,6 +2,18 @@ import {connect} from 'react-redux';
 import MonitorAdminActionHelper from './MonitorAdminActionHelper.js';
 import MonitorAdmin from './MonitorAdmin.jsx';
 
+
+function calcTimeDiff(date1) {
+	const date1Ms = date1.getTime();
+	const date2Ms = (new Date()).getTime();
+
+	let differenceMs = date2Ms - date1Ms;
+	differenceMs = differenceMs/1000;
+	differenceMs = differenceMs/60;
+	const minutes = Math.floor(differenceMs);
+	return minutes;
+}
+
 export const mapStateToProps = ({monitorAdmin : {data, patient}}) => {
 	let allowCreateNew = false;
 	let isListEmpty = false;
@@ -11,6 +23,14 @@ export const mapStateToProps = ({monitorAdmin : {data, patient}}) => {
 	if (data && data.length === 0) {
 		isListEmpty = true;
 	}
+
+	if (data) {
+		for (let i = 0; i < data.length; i++) {
+			let regDate = new Date(data[i].registrationTime);
+			data[i].diffMin = calcTimeDiff(regDate);
+		}
+	}
+
 
 	return {
 		data,
