1fe3097ea589faeb34a048050f5c7aec3d8da61c
zivl <levi.ziv@gmail.com>
2017-07-10 15:44:20 +0300

    writing node server with es6 style + change promotion image

--- admin/server.js
+++ admin/server.js
@@ -1,121 +1,124 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var smsClient = require('twilio')(
+'use strict';
+
+let express = require('express');
+let bodyParser = require('body-parser');
+let smsClient = require('twilio')(
 	'ACd787fd89ceb7d642dd3b739cb72f935c',
 	'31d6775f34be5b66790c87687efa4270'
 );
 
-var sendNotification = require('./fireBase/notification.js');
+let sendNotification = require('./fireBase/notification.js');
 
 
-var app = express();
+let app = express();
 app.use(bodyParser.json());
 
-var queue = [];
-//var notificationTimers = [];
-var numberOfMonitors = 5;
-//var notificationTimeoutInMinutes = 5;
+let queue = [];
+//let notificationTimers = [];
+let numberOfMonitors = 5;
+//let notificationTimeoutInMinutes = 5;
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(function (req, res, next) {
 
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
+	// Website you wish to allow to connect
+	res.setHeader('Access-Control-Allow-Origin', '*');
 
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+	// Request methods you wish to allow
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
 
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+	// Request headers you wish to allow
+	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
+	// Set to true if you need the website to include cookies in the requests sent
+	// to the API (e.g. in case you use sessions)
+	res.setHeader('Access-Control-Allow-Credentials', true);
 
-    // Pass to next layer of middleware
-    next();
+	// Pass to next layer of middleware
+	next();
 });
 
 
 app.use('/dist', express.static(__dirname + '/dist'));
 
-app.post('/notify', function(request, response) {
-    sendNotification(request.body.token);
-    response.json({a: 'hello'});
+app.post('/notify', function (request, response) {
+	sendNotification(request.body.token);
+	response.json({a: 'hello'});
 });
 
-app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/index.html');
+app.get('/', function (request, response) {
+	response.sendFile(__dirname + '/index.html');
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+app.listen(app.get('port'), function () {
+	console.log('Node app is running on port', app.get('port'));
 });
 
-app.post('/notify', function(request, response) {
-    sendNotification(request.body.token);
-    response.json({a: 'hello'});
+app.post('/notify', function (request, response) {
+	sendNotification(request.body.token);
+	response.json({a: 'hello'});
 });
 
-app.get('/testInitData', function(request, response){
-    for (var i=0;i < (numberOfMonitors+2);i++) {
-        requestMonitor({
-            id: '3300876' + i + i + i,
-            phone: '050777877' + i + i + i,
-            fullName: 'Pregnant lady ' +i
-        });
-    }
-   response.json(queue);
+app.get('/testInitData', function (request, response) {
+	for (let i = 0; i < (numberOfMonitors + 2); i++) {
+		requestMonitor({
+			id: '3300876' + i + i + i,
+			phone: '050777877' + i + i + i,
+			fullName: 'Pregnant lady ' + i
+		});
+	}
+	response.json(queue);
 });
 
 /*
-Get entire queue ordered by who is called next
+ Get entire queue ordered by who is called next
  */
-app.get('/queue', function(request, response){
-   response.json(queue);
+app.get('/queue', function (request, response) {
+	response.json(queue);
 });
 
 /*
  Return indication whether there is an available chair
  */
-app.get('/queue/status', function(request, response){
-	let isAvailable = { isAvailable: (queue.length-1 < numberOfMonitors)};
+app.get('/queue/status', function (request, response) {
+	let isAvailable = {isAvailable: (queue.length - 1 < numberOfMonitors)};
 	response.json(isAvailable);
 });
 
 /*
-Add a new user to the end of the queue
+ Add a new user to the end of the queue
  */
-app.post('/queue', function(request, response) {
-	var newRequest = request.body;
-    console.log(newRequest);
+app.post('/queue', function (request, response) {
+	let newRequest = request.body;
+	console.log(newRequest);
 	if (getFromQueueByID(newRequest.id) !== null) {
-		response.status(405).send({ error: "User " + newRequest.id+ " already registered." });
+		response.status(405).send({error: "User " + newRequest.id + " already registered."});
 		return;
-	};
+	}
 	requestMonitor(newRequest);
 	response.json(newRequest);
 });
 
 /*
-check if user exists in queue
-*/
-app.post('/queue/check', function(request, response) {
-    let  userData = request.body;
-    
-    if (getFromQueueByID(userData.id) !== null) {
-		response.status(405).send({ exists: true });
-        return
-	};
-    
-    response.send({exist: false});
+ check if user exists in queue
+ */
+app.post('/queue/check', function (request, response) {
+	let userData = request.body;
+
+	if (getFromQueueByID(userData.id) !== null) {
+		response.status(405).send({exists: true});
+		return
+	}
+	;
+
+	response.send({exist: false});
 });
 
 /*
  Clears the entire queue and all timers.
  */
-app.delete('/queue', function(request, response){
+app.delete('/queue', function (request, response) {
 	queue = [];
 	//for (id in notificationTimers) {
 	//	clearTimeout(notificationTimers[id]);
@@ -126,59 +129,61 @@ app.delete('/queue', function(request, response){
 });
 
 /*
-Update the queue configuration. Set the numberOfMonitors and notificationTimeoutInMinutes parameters
+ Update the queue configuration. Set the numberOfMonitors and notificationTimeoutInMinutes parameters
  */
-app.put('/queue/config', function(request, response){
+app.put('/queue/config', function (request, response) {
 	let configData = request.body;
-	if(configData.numberOfMonitors !== undefined) {
+	if (configData.numberOfMonitors !== undefined) {
 		numberOfMonitors = configData.numberOfMonitors;
 	}
 	/*if(configData.notificationTimeoutInMinutes !== undefined) {
-		notificationTimeoutInMinutes = configData.notificationTimeoutInMinutes;
-	}*/
+	 notificationTimeoutInMinutes = configData.notificationTimeoutInMinutes;
+	 }*/
 	response.json({});
 });
 /*
-Removes a user from the queue
+ Removes a user from the queue
  */
-app.delete('/queue/:id', function(request, response){
-   let id = request.params.id;
-   deleteUser(id);
-   response.json({});
-});
-/*
-Move user a place up in the queue
- */
-app.put('/queue/up/:id', function(request, response){
-   let id = request.params.id;
-   for (index in queue) {
-      if (queue[index].id === id) {
-         if (index == 0) {
-             break;
-            } else {
-             let user = queue[index];
-            // remove from queue
-             queue.splice(index,1);
-             // add back to queue
-            queue.splice(index-1, 0, user);
-         }
-      }
-   }
-   response.json(queue);
-});
-/*
-Move user to the top of the queue
- */
-app.put('/queue/top/:id', function(request, response){
+app.delete('/queue/:id', function (request, response) {
 	let id = request.params.id;
-	for (index in queue) {
+	deleteUser(id);
+	response.json({});
+});
+/*
+ Move user a place up in the queue
+ */
+app.put('/queue/up/:id', function (request, response) {
+	let id = request.params.id;
+	for (let index in queue) {
 		if (queue[index].id === id) {
 			if (index == 0) {
 				break;
-			} else {
+			}
+			else {
 				let user = queue[index];
 				// remove from queue
-				queue.splice(index,1);
+				queue.splice(index, 1);
+				// add back to queue
+				queue.splice(index - 1, 0, user);
+			}
+		}
+	}
+	response.json(queue);
+});
+/*
+ Move user to the top of the queue
+ */
+app.put('/queue/top/:id', function (request, response) {
+	let id = request.params.id;
+	for (let index in queue) {
+		if (queue[index].id === id) {
+			if (index == 0) {
+				break;
+			}
+			else {
+				let user = queue[index];
+				// remove from queue
+				queue.splice(index, 1);
 				// add back to queue
 				queue.splice(0, 0, user);
 			}
@@ -187,59 +192,60 @@ app.put('/queue/top/:id', function(request, response){
 	response.json(queue);
 });
 /*
-Move user down in queue
+ Move user down in queue
  */
-app.put('/queue/down/:id', function(request, response){
-    let id = request.params.id;
-    for (index in queue) {
-        if (queue[index].id === id) {
-            if (index == queue.length-1) {
-               break;
-            } else {
-                let user = queue[index];
-                // remove from queue
-                queue.splice(index,1);
-                // add back to queue
-                queue.splice(index+1, 0, user);
-            }
-        }
-    }
-   response.json(queue);
+app.put('/queue/down/:id', function (request, response) {
+	let id = request.params.id;
+	for (let index in queue) {
+		if (queue[index].id === id) {
+			if (index == queue.length - 1) {
+				break;
+			}
+			else {
+				let user = queue[index];
+				// remove from queue
+				queue.splice(index, 1);
+				// add back to queue
+				queue.splice(index + 1, 0, user);
+			}
+		}
+	}
+	response.json(queue);
 });
 
 /*
-Get first user that has not been notified from queue queue and notify
-*/
-app.put('/queue/notify', function(request, response){
-    // find first user without notification time
-    let user = {};
-    for (index  in queue) {
-        if (queue[index].notificationTime === undefined) {
-            user = queue[index];
-	        notifyUser(user);
-	        break;
-        }
-    }
-    response.json(queue);
+ Get first user that has not been notified from queue queue and notify
+ */
+app.put('/queue/notify', function (request, response) {
+	// find first user without notification time
+	let user = {};
+	for (let index  in queue) {
+		if (queue[index].notificationTime === undefined) {
+			user = queue[index];
+			notifyUser(user);
+			break;
+		}
+	}
+	response.json(queue);
 });
 /****************************************************************************
  * M E T H O D S
-*****************************************************************************/
+ *****************************************************************************/
 
 /*
-Send a request for a monitor, returns true if available and false if queued
+ Send a request for a monitor, returns true if available and false if queued
  */
 function requestMonitor(user) {
 	user.registrationTime = new Date();
 	queue.push(user);
 	/*if (queue.length-1 < numberOfMonitors) {
-		startTimerUser(user);
-        return true;
-    }*/
-    return false;
+	 startTimerUser(user);
+	 return true;
+	 }*/
+	return false;
 }
 /*
-Removes the user properly, both the timeout of the notification and from the queue
+ Removes the user properly, both the timeout of the notification and from the queue
  */
 function deleteUser(id) {
 	// removing timeout in case we have it
@@ -247,17 +253,17 @@ function deleteUser(id) {
 	//	clearTimeout(notificationTimers[id]);
 	//    delete notificationTimers[id];
 	//}
-	for (index in queue) {
+	for (let index in queue) {
 		if (queue[index].id === id) {
-            let user = queue[index];
-            sendNotification(user.token, true);
-			queue.splice(index,1);
+			let user = queue[index];
+			sendNotification(user.token, true);
+			queue.splice(index, 1);
 			break;
 		}
 	}
 }
 /*
-Starts the timer for the user to get to the monitor. sets the end time on the user objectp
+ Starts the timer for the user to get to the monitor. sets the end time on the user objectp
  */
 function startTimerUser(user) {
 	let endTime = new Date();
@@ -266,15 +272,15 @@ function startTimerUser(user) {
 	//notificationTimers[user.id] = timeout;
 }
 /*
-// TODO - use for push notification
+ // TODO - use for push notification
  */
 function notifyUser(user) {
-  startTimerUser(user);
-  sendNotification(user.token);
-  sendSms(user.phone);
+	startTimerUser(user);
+	sendNotification(user.token);
+	sendSms(user.phone);
 }
 /*
-Send SMS to number
+ Send SMS to number
  */
 function sendSms(phone) {
 	let intlPhoneNo = "+972" + phone.substring(1);
@@ -282,26 +288,26 @@ function sendSms(phone) {
 		from: '+18058521995',
 		to: intlPhoneNo,
 		body: "Moni-tor. It's your turn!"
-	}, function(err, message) {
-		if(err) {
+	}, function (err, message) {
+		if (err) {
 			console.error(err.message);
 		}
 	});
 }
 /*
-Callback from the timeout
+ Callback from the timeout
  */
 function timeOutDelete(id) {
-    deleteUser(id);
+	deleteUser(id);
 }
 /*
-Get the user from the queue by ID
+ Get the user from the queue by ID
  */
 function getFromQueueByID(id) {
-   for (let index in queue) {
-      if (queue[index].id === id) {
-         return queue[index];
-      }
-   }
-   return null;
+	for (let index in queue) {
+		if (queue[index].id === id) {
+			return queue[index];
+		}
+	}
+	return null;
 }
